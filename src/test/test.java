14865e3fe08ed9db85b6c645ccf03cf191770bea
尚艺 <shangyi1@yonyou.com>
2023-10-09 13:59:28 +0800

    实现识别文件编码功能 优化主页面

--- src/test/test.java
+++ src/test/test.java
@@ -5,11 +5,13 @@ import org.mozilla.universalchardet.UniversalDetector;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+// 错误的字符串识别脚本 待改正
 public class test {
     public static void main(String[] args) {
         try {
             // 读取文件内容
-            FileInputStream fis = new FileInputStream("C:\\Users\\33718\\Desktop\\sqlTool\\sqlTool\\src\\test\\test2.py");
+            FileInputStream fis = new FileInputStream("C:\\Users\\Administrator\\" +
+                    "Desktop\\init_unmaned_plane_system-master\\sqlTool\\src\\test\\2.txt");
             byte[] data = new byte[fis.available()];
             fis.read(data);
             fis.close();
