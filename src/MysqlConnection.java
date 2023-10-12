f90bfb05efc22686fd8e33404dc040cd900f93d4
shangyi <3371860357@qq.com>
2023-10-12 17:38:00 +0800

    数据库连接池优化

--- src/MysqlConnection.java
+++ src/MysqlConnection.java
@@ -1,59 +1,30 @@
-import java.sql.*;
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+import static java.sql.DriverManager.getConnection;
+
 
 public class MysqlConnection {
-    private static String username;
-    private static  String password;
-    private static String url;
-    private volatile static MysqlConnection mysqlConnection;  // volatile禁止指令重排
     private static Connection connection; // 声明Connection对象
-    private MysqlConnection(String url, String username, String password) throws SQLException {
-        System.out.println(Thread.currentThread().getName()+" 构造方法");
-
-        this.url = url;
-        this.username = username;
-        this.password = password;
-
-        this.connection = DriverManager.getConnection(url, username, password);
+    private MysqlConnection(String url, String username, String password) throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        this.connection = getConnection(url, username, password);
     }
 
-    public static MysqlConnection getSqlConnect(String url, String username, String password) throws SQLException {
-        if (mysqlConnection ==null) {
-            synchronized (MysqlConnection.class){
-                if(mysqlConnection ==null){
-                    mysqlConnection = new MysqlConnection(url,username,password);
-                }
-            }
-        }
 
-        return mysqlConnection;
-    }
+    public static void main(String[] args) throws SQLException, ClassNotFoundException { // 主方法，测试连接
+        String dbAddress = "localhost:3306";
+        String dbname = "sqltool";
+        String user = "root";
+        String password = "shangyi";
+        String type = "mysql";
 
-    public static MysqlConnection getSqlConnect(){
-        return mysqlConnection;
-    }
+        String url = "jdbc:mysql://localhost:3306/sqltool";
 
-    public Connection getConnection(){
-        return connection;
-    }
+        MysqlConnection m = new MysqlConnection(url,user,password);
+        java.sql.Connection con = m.connection;
 
-    public void setDatabaseParams(String url, String username, String password) throws SQLException {
-        // 在这里设置新的数据库连接参数，并重新初始化连接
-        this.url = url;
-        this.username = username;
-        this.password = password;
-
-        connection = DriverManager.getConnection(url, username, password);
-    }
-
-    public static boolean isInstanceCreated() {
-        return mysqlConnection != null;
-    }
-
-    public void setConnection(Connection con){
-        connection = con;
-    }
-
-    public static void main(String[] args) throws SQLException { // 主方法，测试连接
     }
 
 }
